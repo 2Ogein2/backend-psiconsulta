a9f6427891aca3783966e4a606de8e31a139ab32
FernandoMellet <conpalabrassehabla@hotmail.com>
2019-07-04 11:51:23 +0200

    m4

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/psicon', {
+mongoose.connect('mongodb://localhost:27017/psicon', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
